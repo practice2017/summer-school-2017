5ed0260342b8682457bb04be11103d60d57a94be
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2017-07-04 21:01:08 +0500

    common styles

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import './promise-polyfill';
 import { app } from './app';
 
+import 'styles/style.scss';
+
 // Enable progressive web app support (with offline-plugin)
 if (process.env.NODE_ENV === 'production') {
   require('./pwa');
