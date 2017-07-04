e70b6457d4f28373ea83a49bd80fe6dbe2cf80de
Denis Pankov <dspancov@gmail.com>
2017-07-04 20:09:13 +0500

    Added search & details view to routers

--- client/router/index.js
+++ client/router/index.js
@@ -1,15 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '../views/Home'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './../views/Home';
+import Search from './../views/Search';
+import Details from './../views/Details';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: Home
-    }
-  ]
-})
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/search',
+            component: Search
+        },
+        {
+            path: '/city/:id',
+            component: Details
+        }
+    ]
+});
