a780e44ddb38bd768ab1258c8bee5d3e7aef60ea
Nikolay Fedoseev <fedoseevn@gmail.com>
2017-08-04 02:45:49 +0500

    empty page

--- client/index.js
+++ client/index.js
@@ -1,15 +1,2 @@
-import CityCard from './CityCard';
-import axios from 'axios';
-import { apiKey } from './../config.json';
 
-const cityName = 'Perm';
-
-document.getElementById('add_city').onclick = () => {
-    axios.get(`http://api.apixu.com/v1/current.json?key=${apiKey}&q=${cityName}`)
-        .then(res => {
-            const city = new CityCard(res.data);
-            city.render();
-        })
-        .catch(e => console.log('error'));
-};
 
