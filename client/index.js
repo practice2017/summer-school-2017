203ed9fceb3530944723d3bb6f906a12f79906f0
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2017-08-04 14:29:25 +0500

    starter kit

--- client/index.js
+++ client/index.js
@@ -1,11 +1,1 @@
-import './promise-polyfill';
-import {app} from './app';
-
 import 'styles/style.scss';
-
-// Enable progressive web app support (with offline-plugin)
-if (process.env.NODE_ENV === 'production') {
-    require('./pwa');
-}
-
-app.$mount('#app');
